41d5284f844c998751eb6f3ed4918cac05b67965
Steven Elliott <steven.elliott@live.com>
2021-05-30 14:29:50 -0400

    auth works. is updated mongo atlas. converting existing html to work with it

--- models/user.js
+++ models/user.js
@@ -1,58 +1,15 @@
+
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
+var Schema = mongoose.Schema;
 
-var UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-    },
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-});
+userSchema = new Schema( {
+	
+	unique_id: Number,
+	email: String,
+	username: String,
+	password: String,
+	passwordConf: String
+}),
+User = mongoose.model('User', userSchema);
 
-//authenticate input against database
-UserSchema.statics.authenticate = function (email, password, callback) {
-    User.findOne({ email: email })
-        .exec(function (err, user) {
-            if (err) {
-                return callback(err)
-            } else if (!user) {
-                var err = new Error('User not found.');
-                err.status = 401;
-                return callback(err);
-            }
-            bcrypt.compare(password, user.password, function (err, result) {
-                if (result === true) {
-                    return callback(null, user);
-                } else {
-                    return callback();
-                }
-            })
-        });
-}
-
-//hashing a password before saving it to the database
-UserSchema.pre('save', function (next) {
-    var user = this;
-    bcrypt.hash(user.password, 10, function (err, hash) {
-        if (err) {
-            return next(err);
-        }
-        user.password = hash;
-        next();
-    })
-});
-
-
-var User = mongoose.model('User', UserSchema);
 module.exports = User;
