b2bddde49fa5cd77f21bd9e5a00f581657613490
Steven Elliott <steven.elliott@live.com>
2021-06-02 21:20:46 -0400

    changed port code

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ app.use(function (req, res, next) {
 
 
 // listen on port 3000
-app.listen(3000, function () {
-    console.log('Express app listening on port 3000');
-});
+app.listen(process.env.PORT || 5000) 
+//{
+//     console.log('Express app listening on port 3000');
+// });
