660530c2e63c7f2b9690826d1a06bae20eb93b77
Steven Elliott <69441444+elliott-steven@users.noreply.github.com>
2021-06-05 14:34:53 -0400

    Revert "Branch3"

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
-import { CometChatUI } from "./javascript-react-chat-app-master/src/cometchat-pro-react-ui-kit/CometChatWorkspace/src";
-
 
 import "./index.css";
 
 ReactDOM.render(
   <Router>
     <Auth0ProviderWithHistory>
-      <App>
-        <CometChatUI />
-      </App>
+      <App />
     </Auth0ProviderWithHistory>
   </Router>,
   document.getElementById("root")
