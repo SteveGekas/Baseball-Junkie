beb733de95d42bf04506a7b6f41e4805dc941e73
chrisPicano <cpicano25@gmail.com>
2021-06-07 18:38:29 -0400

    Working baseball api

--- src/views/home.js
+++ src/views/home.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from "react";
 
 import { Hero, HomeContent } from "../components";
+import Baseballapi from "./baseballapi"
 
 class Home extends React.Component {
   render() {
     return (
       <Fragment>
         <Hero />
+        <Baseballapi>
         
+        </Baseballapi>
         <HomeContent />
       </Fragment>
     );
