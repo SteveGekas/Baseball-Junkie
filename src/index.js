8a8dcb598725bf7115a4b6347fa443c92803779b
Steven Elliott <steven.elliott@live.com>
2021-06-07 17:49:10 -0400

    heroku

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
-
-import "./index.css";
 
 ReactDOM.render(
   <Router>
